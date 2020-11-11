3fea5437ad42fe11f8938386dc03f0bfb3e88d9a
Su a Kim <58056141+sua-kim@users.noreply.github.com>
2020-11-11 15:38:07 +0900

    Update Register Restaurant.php
    6. 판매 가격 및 메뉴 select box 생성 완료

--- Register Restaurant.php
+++ Register Restaurant.php
@@ -8,34 +8,52 @@
     form {font-size: 1.2rem; color: black;}
 </style>
 
-<h4> &nbsp &nbsp 식당 등록 </h4>
-<br>
+<h4> &nbsp &nbsp <i class="small material-icons"> add_circle</i> 식당 등록 </h4>
+<br><br>
+
 
 <form action = 'Register Restaurant.php' method = 'post'>
+    &nbsp &nbsp &nbsp 1. 식당 이름 <input type = "text" name = "restaurant_name"> <br> <br>
+    &nbsp &nbsp &nbsp 2. 식당 소개 <input type = "text" name = "restaurant_description"> <br> <br>
+    &nbsp &nbsp &nbsp 3. 식당 전화번호 <input placeholder = "&nbsp &nbsp &nbsp &nbsp 입력 예시: 02-123-4567" type = "text" name = "restaurant_phone"> <br> <br>
+    &nbsp &nbsp &nbsp 4. 식당 위치 (도로명 주소) <input type = "text" name = "restaurant_location"> <br> <br>
+    &nbsp &nbsp &nbsp 5. 식당 영업시간 (24시 기준) <input placeholder = "&nbsp &nbsp &nbsp &nbsp 입력 예시: 7:00 - 22:00" type = "text" name = "restaurant_opening"> <br> <br>
+    &nbsp &nbsp &nbsp 6. 판매 메뉴 및 가격
+    <div class = "row">
+        <div class = "input-field col s3">
+            <select class="browser-default" name = "foodtype">
+                <option value="none">메뉴 분류를 선택해주세요</option>
+                <option value="한식">한식</option>
+                <option value="분식">분식</option>
+                <option value="일식">일식</option>
+                <option value="중식">중식</option>
+                <option value="양식">양식</option>
+                <option value="아시안">아시안</option>
+                <option value="카페">카페</option>
+                <option value="주점">주점</option>
+            </select>
+        </div>
+        <div class = "input-field col s3">
+            <input placeholder = "메뉴명" type = "text" name = "menu_name">
+        </div>
+        <div class = "input-field col s3">
+            <input placeholder = "메뉴 가격" type = "number" name = "menu_price">
+        </div>
+    </div>
 
-    &nbsp &nbsp &nbsp 1. 식당 이름 <input type = "text" name = "restaurant_name">
-    &nbsp &nbsp &nbsp 2. 식당 소개 <input type = "text" name = "restaurant_description">
-    &nbsp &nbsp &nbsp 3. 식당 번호 &nbsp >> 입력 예시: 02-123-4567 <input type = "text" name = "restaurant_phone">
-    &nbsp &nbsp &nbsp 4. 식당 위치 (도로명 주소) <input type = "text" name = "restaurant_location">
-    &nbsp &nbsp &nbsp 5. 식당 영업시간 (24h 기준) &nbsp >> 입력 예시: 7:00 - 22:00 <input type = "text" name = "restaurant_opening">
-    &nbsp &nbsp &nbsp 6. 판매 메뉴 및 가격 &nbsp <textarea name="Text1" cols="40" rows="10"> 음식명과 가격을 띄어쓰기로 구분해주세요 >> 입력 예시: 김밥 3000 </textarea>
-    <br> <br>
     &nbsp &nbsp &nbsp <input type = "submit">
 </form>
 
+
+
 <?php
-    echo $_POST["restaurant_name"];
-    echo $_POST["restaurant_description"];
-    echo $_POST["restaurant_phone"];
-    echo $_POST["restaurant_location"];
-    echo $_POST["restaurant_opening"];
+    //echo $_POST["restaurant_name"];
+    //echo $_POST["restaurant_description"];
+    //echo $_POST["restaurant_phone"];
+    //echo $_POST["restaurant_location"];
+    //echo $_POST["restaurant_opening"];
     echo $_POST["menu_price"];
-
 ?>
-
-
-
-
 
 </body>
 </html>
