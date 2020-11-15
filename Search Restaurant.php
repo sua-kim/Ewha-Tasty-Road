0869dd232e49655dfbe8e0c8e1cde530b11450fc
Su a Kim <58056141+sua-kim@users.noreply.github.com>
2020-11-16 03:35:29 +0900

    Update Search Restaurant.php
    검색 조건 추가 완료

--- Search Restaurant.php
+++ Search Restaurant.php
@@ -16,32 +16,83 @@
 <form action = "Search Restaurant.php", method = "post">
     <h6> &nbsp &nbsp &nbsp <i class = "tiny material-icons"> details </i> 검색 기준 </h6>
     <div class = "row" name = "searchOption">
-        &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "restNameOption"/><span><black>식당</black></span> </label>
-        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "typeOption"/><span><black>음식종류</black></span></label>
-        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "menuOption"/><span><black>메뉴</black></span></label>
-        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "priceOption"/><span><black>가격대</black></span></label>
+        &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "restaurant" value = "restaurant"/><span><black>식당</black></span> </label>
+        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "foodtype" value = "foodtype"/><span><black>음식종류</black></span></label>
+        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "menu" value = "menu"/><span><black>메뉴</black></span></label>
+        &nbsp &nbsp &nbsp <label><input type = "checkbox" name = "price" value = "price"/><span><black>가격대</black></span></label>
+        &nbsp &nbsp &nbsp <label><input type = "submit" name = "선택 완료" value = "선택 완료">
     </div>
-    <br><br>
-    <?php
-        $restNameOption = $_POST['restNameOption'];
-        $typeOption = $_POST['typeOption'];
-        $menuOption = $_POST['menuOption'];
-        $priceOption = $_POST['priceOption'];
-    ?>
-    <div>
-    <?php 
-        if (!empty($_POST['restNameOption'])) { ?>
-        <div class = "row">
-            <div class = "input-field col s3">
-            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <black>식당 이름</black>: <input type = "text" name = "restaurant_name">
-            </div>
-            &nbsp &nbsp &nbsp <input type = "submit" value = '검색'>
-        </div>
-        <?php } ?>
-    </div>
-    <input type="checkbox" name="color" value="red">
-    <p><input type="submit" value="Submit"> <input type="reset" value="Reset"></p>
 </form>
+<br>
+<?php
+    // options are received in variables through '$_post', if not checked save as null value
+    $restaurant = isset($_POST['restaurant']) ? $_POST['restaurant'] : '';
+    $foodtype = isset($_POST['foodtype']) ? $_POST['foodtype'] : '';
+    $menu = isset($_POST['menu']) ? $_POST['menu'] : '';
+    $price = isset($_POST['price']) ? $_POST['price'] : '';
+?>
+
+<form>
+    <?php if (!empty($restaurant)) { // display the input form when selecting '식당' option ?>
+        <!-- create input text box to get name of restaurant -->
+        <div class = "row">
+            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <black>식당 이름 : </black>
+            <div class = "input-field inline">
+            &nbsp &nbsp &nbsp &nbsp <input type = "text" name = "restaurant_name">
+            </div>
+        </div>
+    <?php } ?>
+    <?php if (!empty($foodtype)) { // display the input form when selecting '음식종류' option ?>
+        <!-- create selection box to get foodtype -->
+        <div class = "row">
+            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <black>음식 종류 : </black>
+            <div class = "input-field inline">
+                <select class="browser-default" name = "foodtype_id">
+                    <option value="none">메뉴 분류를 선택해주세요</option>
+                    <option value="한식">한식</option>
+                    <option value="분식">분식</option>
+                    <option value="일식">일식</option>
+                    <option value="중식">중식</option>
+                    <option value="양식">양식</option>
+                    <option value="아시안">아시안</option>
+                    <option value="카페">카페</option>
+                    <option value="주점">주점</option>
+                </select>
+            </div>
+        </div>
+    <?php } ?>
+    <?php if (!empty($menu)) { // display the input form when selecting '메뉴' option ?>
+        <!-- create input text box to get menu -->
+        <div class = "row">
+            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <black>메뉴명 : </black>
+            <div class = "input-field inline">
+            &nbsp &nbsp &nbsp &nbsp <input type = "text" name = "menu_name">
+            </div>
+        </div>
+    <?php } ?>
+    <?php if (!empty($price)) { // display the input form when selecting '가격대' option ?>
+        <!-- create input number box to get minimum and maximum price -->
+        <div class = "row">
+            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <black>가격대 : </black>
+            <div class="input-field inline">
+                <input placeholder="  최소" type="number" name="lowest_price">
+            </div>
+            <div class = "input-field inline">
+            &nbsp &nbsp <black> ~ </black> &nbsp &nbsp
+            </div>
+            <div class="input-field inline">
+                <input placeholder="  최대" type="number" name="highest_price">
+            </div>
+        </div>
+    <?php } ?>
+    <?php if (!empty($restaurant) || !empty($foodtype) || !empty($menu) || !empty($price)) { ?>
+        &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <input type = "submit" value = '검색'>
+    <?php } ?>
+
+
+</form>
+
+
 
 </body>
 </html>
