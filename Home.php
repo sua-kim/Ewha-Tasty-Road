854c98f1ed2f6a654a908ae81103d66f7b35ef9e
Su a Kim <58056141+sua-kim@users.noreply.github.com>
2020-11-19 22:37:11 +0900

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,16 @@
+<?php
+    include 'includes/head.php';
+    include 'includes/header.php';
+?>
+
+<style type = "text/css">
+    h4 {color: #004d40; text-align: center;}
+    h6 {color: #212121; text-align: center;}
+</style>
+
+<?php
+    echo "<h4><img width='80' height='80' src='img-symbol01.gif'/></h4>"
+?>
+<h4> 이화 벗들의 맛집 </h4> <br>
+<h6>이대/신촌 맛집 정보를 벗들과 공유하는 페이지입니다 &#127834; &#127838; &#127847;</h6>
+
